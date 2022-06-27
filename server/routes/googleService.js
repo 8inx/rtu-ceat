dfd174f1293adaf5b94ed14bcfec84ff484cc376
sinyx21 <dale.acebo@gmail.com>
2022-06-27 15:35:46 +0800

    add server backend

---
+++ server/routes/googleService.js
@@ -0,0 +1,18 @@
+const {google} = require('googleapis');
+require('dotenv').config()
+
+const googleService = ((req, res, next)=>{
+
+    const scopes = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.file']
+    const auth = new google.auth.JWT(
+        process.env.CLIENT_ID,
+        null,
+        process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+        scopes,
+    )
+
+    req.drive = google.drive({version: 'v3',auth})
+    next();
+})
+
+module.exports = googleService;
